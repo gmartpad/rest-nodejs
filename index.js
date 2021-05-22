b720cc7444f6e10d199d781b9baa52c9af6f2a36
gmartpad <gabrielmpwork@gmail.com>
2021-05-22 20:22:25 -0300

    feat: Criando nossa base de dados com MySQL

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao');
 
-const app = customExpress();
+conexao.connect(erro => {
+    
+    if(erro) {
 
-const port = 3000;
+        console.log(erro)
+    
+    } else {
+        
+        console.log(`Conexão ao banco de dados realizada com sucesso!`)
+        
+        const app = customExpress();
+        
+        const port = 3000;
+        
+        // O servidor estará rodando e escutando na porta 3000
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta ${port}`)
+        })
+    
+    }
 
-// O servidor estará rodando e escutando na porta 3000
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
 })
