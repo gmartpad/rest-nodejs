2445ce139a8b9541ad2e4bb4d62319aba82f8328
gmartpad <gabrielmpwork@gmail.com>
2021-05-22 05:31:31 -0300

    feat: rota para criar atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,6 +1,11 @@
 module.exports = app => {
     // Disponibilizando um Hello, World! para requisições GET executadas no endpoint /
-    app.get('/atendimentos', function (req, res) {
+    app.get('/atendimentos', (req, res) => {
         res.send('Você está na rota de atendimentos!')
     })
+
+    app.post('/atendimentos', (req, res) => {
+        res.send('Você está na rota de atendimentos e está realizando um POST')
+    })
+
 }
