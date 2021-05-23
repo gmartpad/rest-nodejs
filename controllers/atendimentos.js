180e7718713fb5af0f5ccdaeaafa23b920af9df2
gmartpad <gabrielmpwork@gmail.com>
2021-05-22 22:58:08 -0300

    feat: Status HTTP

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -10,10 +10,7 @@ module.exports = app => {
 
         const atendimento = req.body;
 
-        Atendimento.adiciona(atendimento);
-
-        res.send('Você está na rota de atendimentos e está realizando um POST');
-
+        Atendimento.adiciona(atendimento, res);
 
     })
 
